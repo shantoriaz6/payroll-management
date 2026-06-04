2a25f3c2624c715b79e5f14466c73b5405bca59c
shantoriaz6 <22103285@iubat.edu>
2026-06-04 09:46:33 +0600

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>  
+      <h1 className='text-3xl font-bold text-center mt-10'>Bin Mishal</h1>  
+   </div>
+
+  )
+}
+
+export default App
